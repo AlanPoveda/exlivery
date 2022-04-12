38a24e94b6a4c5f0a73142f65a8263ac001a6303
Alan Poveda <alsepoveda@gmail.com>
2022-04-11 22:55:26 -0300

    Create a User struct whit build function

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -3,4 +3,16 @@ defmodule Exlivery.Users.User do
   @enforce_keys @keys
   defstruct @keys
 
+  #Normalmente num struct tem uma função de build, para não ter que digitar toda vez, pedaço por pedaço
+  def build(name, email, cpf, age) when age >= 18 do
+    {:ok, %__MODULE__{
+      name: name,
+      email: email,
+      cpf: cpf,
+      age: age
+    }}
+  end
+
+  def build(_name, _email, _cpf, _age), do: {:error, "Invalid parameters"}
+
 end
