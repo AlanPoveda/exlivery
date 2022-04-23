a21c24968536826c8ee816e935b7a499ab1204c2
Alan Poveda <alsepoveda@gmail.com>
2022-04-22 23:38:39 -0300

    Create or update orders, created, but no t finished

---
+++ lib/orders/create_or_update.ex
@@ -0,0 +1,8 @@
+defmodule Exlivery.Orders.CreateOrUpdate do
+  alias Exlivery.Users.User, as: UserAgent
+  alias Exlivery.Order.Agent, as: OrderAgent
+
+  def call(%{cpf: cpf, items: items}) do
+
+  end
+end
