e95717487b60d56652411f742807f8d917b0b486
Alan Poveda <alsepoveda@gmail.com>
2022-04-24 21:46:03 -0300

    UserAgent test, function get

--- test/users/agent_test.exs
+++ test/users/agent_test.exs
@@ -15,4 +15,37 @@ defmodule Exlivery.Users.AgentTest do
     end
   end
 
+  describe "get/1" do
+    test "When the user is found, return the user" do
+      UserAgent.start_link(%{})
+
+      :user
+      |> build(cpf: "12345678990")
+      |> UserAgent.save()
+
+      response = UserAgent.get("12345678990")
+
+      assert response ==
+               {:ok,
+                %Exlivery.Users.User{
+                  address: "Rua opa 23",
+                  age: 26,
+                  cpf: "12345678990",
+                  email: "Alan@poveda.com",
+                  name: "Alan"
+                }}
+    end
+
+    test "When the user is not found, return a error message" do
+      UserAgent.start_link(%{})
+
+      :user
+      |> build(cpf: "12345678990")
+      |> UserAgent.save()
+
+      response = UserAgent.get("123456789")
+
+      assert response == {:error, "User not found"}
+    end
+  end
 end
