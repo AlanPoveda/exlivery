b4b6e8dc306703ea8fd628a8d8c15ec7877a25c0
Alan Poveda <alsepoveda@gmail.com>
2022-05-22 22:21:14 -0300

    Finish exlivery app

--- test/orders/create_or_update_test.exs
+++ test/orders/create_or_update_test.exs
@@ -1,31 +1,42 @@
 defmodule Exlivery.Orders.CreateOrUpdateTest do
-  use ExUnit.case
+  use ExUnit.Case
 
   import Exlivery.Factory
 
-  alias Exlivery.Users.Agent, as: UserAgent
   alias Exlivery.Orders.CreateOrUpdate
+  alias Exlivery.Users.Agent, as: UserAgent
 
   describe "call/1" do
-    cpf = "12345678900"
-    user = build(:user, :cpf = cpf)
+    setup do
+      cpf = "12345678900"
+      user = build(:user, cpf: cpf)
 
-    UserAgent.save(user)
+      Exlivery.start_agents()
+      UserAgent.save(user)
 
-    item1 = %{
-      category: :pizza,
-      description: "Pizza de frango com catupiry",
-      quantity: 1,
-      unit_price: "42.4"
+      item1 = %{
+        category: :pizza,
+        description: "Pizza de frango com catupiry",
+        quantity: 1,
+        unit_price: "42.4"
+      }
 
-    }
-    item1 = %{
-      category: :pizza,
-      description: "Pizza de frango com catupiry",
-      quantity: 1,
-      unit_price: "42.4"
+      item2 = %{
+        category: :pizza,
+        description: "Pizza de frango com calabreza",
+        quantity: 1,
+        unit_price: "42.2"
+      }
 
-    }
+      {:ok, cpf: cpf, item1: item1, item2: item2}
+    end
 
+    test "when all param is valid, saves the order", %{cpf: cpf, item1: item1, item2: item2} do
+      params = %{cpf: cpf, items: [item1, item2]}
+
+      response = CreateOrUpdate.call(params)
+
+      assert {:ok, _uuid} = response
+    end
   end
 end
